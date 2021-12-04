4ca1818cc66b012c55cade72d9cce74968fe8dac
That is me <trab.die@gmail.com>
2021-12-04 09:19:59 -0500

    class dice

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,4 +1,20 @@
 public class Dice {
 
+    // a number of N random-tossed dies.
+Integer numbOfDice;
+
+    public Dice(Integer numOfDice) {
+        this.numbOfDice = numOfDice;
+    }
+
+    public Integer getNumbOfDice() {
+        return numbOfDice;
+    }
+
+    public void setNumbOfDice(Integer numbOfDice) {
+        this.numbOfDice = numbOfDice;
+    }
+
+
 
 }
