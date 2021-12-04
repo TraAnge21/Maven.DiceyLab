118b682b6925022ef45dc04c73cbd2b91dcdab57
That is me <trab.die@gmail.com>
2021-12-04 09:46:35 -0500

    class dice

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -5,18 +5,18 @@ public class Dice {
     // a number of N random-tossed dies.
  private Integer numOfDice;
 
-//
-
-
+// constructor
     public Dice(Integer numOfDice) {
         this.numOfDice = numOfDice;
 
     }
 
+    // getter
     public Integer getNumOfDice() {
         return numOfDice;
     }
 
+    // setter
     public void setNumOfDice(Integer numOfDice) {
         this.numOfDice = numOfDice;
     }
@@ -28,8 +28,7 @@ public class Dice {
         int max = 6;
         Integer sumTossDice =0;
         // random toss of dice and their sum
-        for (int j = 0; j <= numOfDice ; j++) {
-
+        for (int j = 1; j <= numOfDice ; j++) {
             sumTossDice+= rand.nextInt(max-min+1)+1;
 
         }
