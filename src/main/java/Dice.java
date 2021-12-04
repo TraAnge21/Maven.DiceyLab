d1091b6c4aef7bd8baacce5439d3538927dcb549
That is me <trab.die@gmail.com>
2021-12-04 09:44:19 -0500

    class sum of toss of dice

--- src/main/java/Dice.java
+++ src/main/java/Dice.java
@@ -1,20 +1,42 @@
+import java.util.Random;
+
 public class Dice {
 
     // a number of N random-tossed dies.
-Integer numbOfDice;
+ private Integer numOfDice;
+
+//
+
 
     public Dice(Integer numOfDice) {
-        this.numbOfDice = numOfDice;
+        this.numOfDice = numOfDice;
+
     }
 
-    public Integer getNumbOfDice() {
-        return numbOfDice;
+    public Integer getNumOfDice() {
+        return numOfDice;
     }
 
-    public void setNumbOfDice(Integer numbOfDice) {
-        this.numbOfDice = numbOfDice;
+    public void setNumOfDice(Integer numOfDice) {
+        this.numOfDice = numOfDice;
     }
 
+// a method to calculate the sum of dice after several toss
+    public Integer sumOfToss () {
+        Random rand = new Random();
+        int min = 1;
+        int max = 6;
+        Integer sumTossDice =0;
+        // random toss of dice and their sum
+        for (int j = 0; j <= numOfDice ; j++) {
+
+            sumTossDice+= rand.nextInt(max-min+1)+1;
+
+        }
+        return sumTossDice;
+
+
+    }
 
 
 }
